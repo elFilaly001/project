46fa7330e1b1ede336308225e622e03c5bfc04bf
Zakaria MOUCHTATI <z.mouchtati@gmail.com>
2025-11-05 12:23:38 +0100

    correct the errors build

--- app/[lang]/(brand)/dashboard/page.tsx
+++ app/[lang]/(brand)/dashboard/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import DashboardClient from "../../../components/pages/DashboardClient";
+import DashboardClient from "@/components/pages/DashboardClient";
 import Loader from '@/components/Loader';
 
 type Params = { lang: string };
